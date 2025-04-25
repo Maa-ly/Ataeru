6bf42e04754d4d307db833fc6b9c404b0d666842
Maa-ly <lydiaahenkorahgyamfi@gmail.com>
2025-04-24 20:51:08 -0700

    reward mech started

---
+++ smartContract/src/core/reward.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+
+contract RewardContract {
+uint256 totalReward;
+uint256 lastTimeclaimed;
+uint256 rewardPerShare;
+address[] token;
+
+function isRewardToken() public {}
+
+    function addReward() public {}
+
+    function earned() public {}
+
+    function rewardPerUser public{}
+
+    function claim() public {}
+
+    function updateReward() public {}
+}
